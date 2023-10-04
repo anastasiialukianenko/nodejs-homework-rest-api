deff4bf93f43e5b9274d143a7b5480a589c57cfd
anastasiialukianenko <121677643+anastasiialukianenko@users.noreply.github.com>
2023-10-04 11:45:12 +0200

    edit

--- helpers/validateData.js
+++ helpers/validateData.js
@@ -1,23 +1,11 @@
-import Joi from 'joi';
+import { contactAddSchema } from "../helpers/schems.js"
 import { HttpError } from "../helpers/index.js";
 
-
-const contactAddSchema = Joi.object({
-        name: Joi.string().required().messages({
-    'any.required': `Missing required "name" field`,
-  }),
-        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'org'] } }).required().messages({
-    'any.required': `Missing required "email" field`,
-  }),
-        phone: Joi.string().min(2).required().messages({
-    'any.required': `Missing required "phone" field`,
-  }),
-})
 
 export const validateData = (req, res, next) => {
   try {
     if (!Object.keys(req.body).length) {
-      throw HttpError(400, 'All fields are empty');
+      throw HttpError(400, "missing fields");
     }
     const { error } = contactAddSchema.validate(req.body);
     if (error) {
