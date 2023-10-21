54f899900dbaded23a950a1a859e2c8901c09aea
anastasiialukianenko <121677643+anastasiialukianenko@users.noreply.github.com>
2023-10-21 17:28:18 +0200

    add avatar

---
+++ middlewares/upload.js
@@ -0,0 +1,34 @@
+import multer from "multer";
+import path from "path";
+
+const destination = path.resolve("tmp");
+
+const storage = multer.diskStorage(
+    {
+        destination,
+        filename(req, file, cb) {
+            const uniquePreffix = `${Date.now()}_${Math.random() * 1E9}`;
+            const fileName = `${uniquePreffix}_${file.originalname}`;
+               cb(null, fileName);
+        }
+    }
+)
+
+const limits = {
+    fileSize: 5 * 1024 *1024
+}
+
+// const filter = (req, file, cb) => {
+//                 if (file.originalname.split('.').pop() === "exe") {
+//                 cb(new Error('File extention is nor allow'));
+//                 }
+//               cb(null, true);
+// }
+
+const upload = multer({
+    storage,
+    limits,
+    // fileFilter,
+})
+
+export default upload;
